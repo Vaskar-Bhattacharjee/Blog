c1c2815c74d6ceecd57a1531e088dc1f056d4e2d
Vaskar-Bhattacharjee <vaskarbhattacharjee03@gmail.com>
2024-10-28 17:16:54 +0600

    add

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -25,7 +25,7 @@ function Login() {
       }
     } catch (err) {
       console.log('Error object:', err); // Optional: Keep for further debugging if needed
-      setError('Incorrect email or password. Please try again.');
+      setError(' Appwrite Authentication Failed. Incorrect email or password. Please try again.');
     }
   };
   
